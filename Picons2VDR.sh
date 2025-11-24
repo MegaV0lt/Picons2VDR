b9e36718951e32cbe86a09f3f0bb92d7896f8cd8
MegaV0lt <Stefan.Blochberger@gmx.de>
2025-11-24 13:11:42 +0100

    Update VERSION

--- Picons2VDR.sh
+++ Picons2VDR.sh
@@ -11,7 +11,7 @@
 # Die Logos werden im PNG-Format erstellt. Die Größe und den optionalen Hintergrund
 # kann man in der *.conf einstellen.
 # Das Skript am besten ein mal pro Woche ausführen (/etc/cron.weekly)
-VERSION=251120  # Version des Skripts
+VERSION=251124  # Version des Skripts
 
 # Sämtliche Einstellungen werden in der *.conf vorgenommen.
 # ---> Bitte ab hier nichts mehr ändern! <---
